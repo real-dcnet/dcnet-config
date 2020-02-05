a95bbfea82c07fdf54a655d634b7f5bb4bb6e8a4
William Reed <reed226@purdue.edu>
2020-02-05 17:18:19 +0000

    Fix host numbers in arping scripts

--- dcnet00arping.sh
+++ dcnet00arping.sh
@@ -1,7 +1,7 @@
 sudo ip netns exec host1 arping -U -I eno2 128.10.126.51 -w 5
-sudo ip netns exec host1 arping -U -I enp1s0f0 128.10.126.52 -w 5
-sudo ip netns exec host1 arping -U -I enp1s0f1 128.10.126.53 -w 5
-sudo ip netns exec host1 arping -U -I enp132s0f0 128.10.126.54 -w 5
-sudo ip netns exec host1 arping -U -I enp132s0f1 128.10.126.55 -w 5
-sudo ip netns exec host1 arping -U -I enp132s0f2 128.10.126.56 -w 5
-#sudo ip netns exec host1 arping -U -I enp132s0f3 128.10.126.57 -w 5
+sudo ip netns exec host2 arping -U -I enp1s0f0 128.10.126.52 -w 5
+sudo ip netns exec host3 arping -U -I enp1s0f1 128.10.126.53 -w 5
+sudo ip netns exec host4 arping -U -I enp132s0f0 128.10.126.54 -w 5
+sudo ip netns exec host5 arping -U -I enp132s0f1 128.10.126.55 -w 5
+sudo ip netns exec host6 arping -U -I enp132s0f2 128.10.126.56 -w 5
+#sudo ip netns exec host7 arping -U -I enp132s0f3 128.10.126.57 -w 5
--- dcnet30arping.sh
+++ dcnet30arping.sh
@@ -1,7 +1,7 @@
 sudo ip netns exec host1 arping -U -I eno2 128.10.126.75 -w 5
-sudo ip netns exec host1 arping -U -I enp1s0f0 128.10.126.76 -w 5
-sudo ip netns exec host1 arping -U -I enp1s0f1 128.10.126.77 -w 5
-sudo ip netns exec host1 arping -U -I enp132s0f0 128.10.126.78 -w 5
-sudo ip netns exec host1 arping -U -I enp132s0f1 128.10.126.79 -w 5
-sudo ip netns exec host1 arping -U -I enp132s0f2 128.10.126.80 -w 5
-sudo ip netns exec host1 arping -U -I enp132s0f3 128.10.126.81 -w 5
+sudo ip netns exec host2 arping -U -I enp1s0f0 128.10.126.76 -w 5
+sudo ip netns exec host3 arping -U -I enp1s0f1 128.10.126.77 -w 5
+sudo ip netns exec host4 arping -U -I enp132s0f0 128.10.126.78 -w 5
+sudo ip netns exec host5 arping -U -I enp132s0f1 128.10.126.79 -w 5
+sudo ip netns exec host6 arping -U -I enp132s0f2 128.10.126.80 -w 5
+sudo ip netns exec host7 arping -U -I enp132s0f3 128.10.126.81 -w 5
