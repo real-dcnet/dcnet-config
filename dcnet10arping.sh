9f280fe8b81237905fd446668f49e25705401d92
William Reed <reed226@purdue.edu>
2020-02-26 17:01:34 +0000

    Add ping commands to arping

--- dcnet10arping.sh
+++ dcnet10arping.sh
@@ -1,7 +1,14 @@
 sudo ip netns exec host1 arping -U -I eno2 128.10.126.59 -w 5
+sudo ip netns exec host1 ping -c 5 128.10.126.51
 sudo ip netns exec host2 arping -U -I enp1s0f0 128.10.126.60 -w 5
+sudo ip netns exec host2 ping -c 5 128.10.126.51
 sudo ip netns exec host3 arping -U -I enp1s0f1 128.10.126.61 -w 5
+sudo ip netns exec host3 ping -c 5 128.10.126.51
 sudo ip netns exec host4 arping -U -I enp132s0f0 128.10.126.62 -w 5
+sudo ip netns exec host4 ping -c 5 128.10.126.51
 sudo ip netns exec host5 arping -U -I enp132s0f1 128.10.126.63 -w 5
+sudo ip netns exec host5 ping -c 5 128.10.126.51
 sudo ip netns exec host6 arping -U -I enp132s0f2 128.10.126.64 -w 5
+sudo ip netns exec host6 ping -c 5 128.10.126.51
 sudo ip netns exec host7 arping -U -I enp132s0f3 128.10.126.65 -w 5
+sudo ip netns exec host7 ping -c 5 128.10.126.51
--- dcnet20arping.sh
+++ dcnet20arping.sh
@@ -1,7 +1,14 @@
 sudo ip netns exec host1 arping -U -I eno2 128.10.126.67 -w 5
+sudo ip netns exec host1 ping -c 5 128.10.126.51
 sudo ip netns exec host2 arping -U -I enp1s0f0 128.10.126.68 -w 5
+sudo ip netns exec host2 ping -c 5 128.10.126.51
 sudo ip netns exec host3 arping -U -I enp1s0f1 128.10.126.69 -w 5
+sudo ip netns exec host3 ping -c 5 128.10.126.51
 sudo ip netns exec host4 arping -U -I enp132s0f0 128.10.126.70 -w 5
+sudo ip netns exec host4 ping -c 5 128.10.126.51
 sudo ip netns exec host5 arping -U -I enp132s0f1 128.10.126.71 -w 5
+sudo ip netns exec host5 ping -c 5 128.10.126.51
 sudo ip netns exec host6 arping -U -I enp132s0f2 128.10.126.72 -w 5
+sudo ip netns exec host6 ping -c 5 128.10.126.51
 sudo ip netns exec host7 arping -U -I enp132s0f3 128.10.126.73 -w 5
+sudo ip netns exec host7 ping -c 5 128.10.126.51
